1d4454cc8b65be8a2491a1c518c6fa9f613cd3c5
Angielf <4748510+Angielf@users.noreply.github.com>
2020-11-27 12:06:31 +0300

    DopSeeder

---
+++ database/seeds/DopSeeder.php
@@ -0,0 +1,463 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DopSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user_info = array(
+            array(
+                "loginOrg" => "add01_001",
+                "nameOrg" => "МКУ Аннинский центр дополнительного образования \"РИТМ\"",
+                "passOrg" => "sg9t7ne7",
+                "district" => 1,
+            ),
+            array(
+                "loginOrg" => "add01_002",
+                "nameOrg" => "МКУ ДО \"Аннинская станция юных техников\"",
+                "passOrg" => "3f2hskcb",
+                "district" => 1,
+            ),
+            array(
+                "loginOrg" => "add02_001",
+                "nameOrg" => "МБУ ДО Бобровский детско-юношеский центр \"Радуга\"",
+                "passOrg" => "54hnb76d",
+                "district" => 2,
+            ),
+            array(
+                "loginOrg" => "add02_002",
+                "nameOrg" => "МКУ ДО Бобровская станция юных натуралистов",
+                "passOrg" => "rdef552f",
+                "district" => 2,
+            ),
+            array(
+                "loginOrg" => "add03_001",
+                "nameOrg" => "МКУ ДО \"Богучарский районный Центр детского творчества\"",
+                "passOrg" => "29y46kst",
+                "district" => 3,
+            ),
+            array(
+                "loginOrg" => "add04_001",
+                "nameOrg" => "МБУ ДО Борисоглебский центр внешкольной работы БГО",
+                "passOrg" => "p3tpgrv3",
+                "district" => 4,
+            ),
+            array(
+                "loginOrg" => "add04_002",
+                "nameOrg" => "МБУ ДО БГО Центр \"Социальная адаптация молодежи\"",
+                "passOrg" => "fzxkj575",
+                "district" => 4,
+            ),
+            array(
+                "loginOrg" => "add04_003",
+                "nameOrg" => "МБУ ДО \"Борисоглебский детский оздоровительно-образовательный центр \"Дружба\"",
+                "passOrg" => "z94hhrb",
+                "district" => 4,
+            ),
+            array(
+                "loginOrg" => "add05_001",
+                "nameOrg" => "МБОУ ДО Бутурлиновский Дом детского творчества",
+                "passOrg" => "t2mseajh",
+                "district" => 5,
+            ),
+            array(
+                "loginOrg" => "add06_001",
+                "nameOrg" => "Муниципальное казенное образовательное учреждение дополнительного образования детей «Станция юных натуралистов Верхнемамонского муниципального района Воронежской области»",
+                "passOrg" => "fnjhrzk2",
+                "district" => 6,
+            ),
+            array(
+                "loginOrg" => "add06_002",
+                "nameOrg" => "МКУ ДО \"Дворец детей и юношества\"",
+                "passOrg" => "zxzehybt",
+                "district" => 6,
+            ),
+            array(
+                "loginOrg" => "add07_001",
+                "nameOrg" => "МКУ ДО \"Дом творчества детей и подростков\"",
+                "passOrg" => "jd73rvpg",
+                "district" => 7,
+            ),
+            array(
+                "loginOrg" => "add08_001",
+                "nameOrg" => "МКУ ДО \"Воробьевский Центр развития творчества детей и юношества\"",
+                "passOrg" => "97hckcze",
+                "district" => 8,
+            ),
+            array(
+                "loginOrg" => "add35_001",
+                "nameOrg" => "МБУ ДО Центр дополнительного образования \"Реальная школа\"",
+                "passOrg" => "6ms99zas",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "add35_002",
+                "nameOrg" => "МБУ ДО Центр развития творчества детей и юношества \"Спутник\"",
+                "passOrg" => "v7h5x5xz",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "add35_003",
+                "nameOrg" => "МБУ ДО Станция юных техников №3",
+                "passOrg" => "hxmrcgce",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "add35_004",
+                "nameOrg" => "МБУ ДО Центр развития творчества детей и юношества \"Крылатый\"",
+                "passOrg" => "ktke53va",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "add37_001",
+                "nameOrg" => "МБУ ДО \"Городская станция юных туристов\"",
+                "passOrg" => "rzd2rp5n",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "add37_002",
+                "nameOrg" => "МБУ ДО Центр дополнительного образования \"Созвездие\"",
+                "passOrg" => "xbycg5ka",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "add37_003",
+                "nameOrg" => "МБОУ ДО \"Городской центр профессиональной ориентации обучающихся\"",
+                "passOrg" => "rbyydb9h",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "add37_004",
+                "nameOrg" => "МБУ ДО Дом пионеров и школьников",
+                "passOrg" => "m7zy8mmg",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "add37_005",
+                "nameOrg" => "МБУ ДО \"Детский эколого-биологический центр \"Росток\"",
+                "passOrg" => "f9u7g4ne",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "add40_001",
+                "nameOrg" => "ГБУ ДО ВО «Центр инженерных компетенций детей и молодежи «Кванториум»",
+                "passOrg" => "f84d5t8y",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "add40_002",
+                "nameOrg" => "МБУ ДО Центр развития творчества детей и юношества \"Радуга\"",
+                "passOrg" => "frmmz8m9",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "add39_001",
+                "nameOrg" => "МБУ ДО Дом детского творчества",
+                "passOrg" => "vk9fzzdj",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "add39_002",
+                "nameOrg" => "МБУ ДО Детско-юношеский центр",
+                "passOrg" => "fnrdretc",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "add39_003",
+                "nameOrg" => "МБУ ДО Центр дополнительного образования \"Родник\"",
+                "passOrg" => "7nffmzmg",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "add39_004",
+                "nameOrg" => "МБУ ДО Центр научно-технического творчества детей и юношества \"Техноград\"",
+                "passOrg" => "naz9n47x",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "add36_001",
+                "nameOrg" => "ГБУ ДО ВО \"Воронежская областная станция юных натуралистов\"",
+                "passOrg" => "fjjtzug5",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "dd36_002",
+                "nameOrg" => "МБУ ДО Дворец творчества детей и молодежи",
+                "passOrg" => "p47tetv8",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "add36_003",
+                "nameOrg" => "МБУ ДО Центр развития творчества детей и юношества",
+                "passOrg" => "g5bsu57f",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "add36_004",
+                "nameOrg" => "МБУ ДО Дом детства и юношества",
+                "passOrg" => "tr4dvvsg",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "add11_001",
+                "nameOrg" => "МКУ ДО \"Грибановский детско-юношеский центр\" Грибановского муниципального района",
+                "passOrg" => "h5mfe3ed",
+                "district" => 11,
+            ),
+            array(
+                "loginOrg" => "add11_002",
+                "nameOrg" => "МКУ ДО \"Грибановский центр детского творчества\" Грибановского муниципального района",
+                "passOrg" => "8922x2na",
+                "district" => 11,
+            ),
+            array(
+                "loginOrg" => "add12_001",
+                "nameOrg" => "МБОО ДО Центр творчества",
+                "passOrg" => "ztcsr2m9",
+                "district" => 12,
+            ),
+            array(
+                "loginOrg" => "add13_001",
+                "nameOrg" => "МКУ ДО \"Центр развития творчества детей и юношества\"",
+                "passOrg" => "u6sy3j49",
+                "district" => 13,
+            ),
+            array(
+                "loginOrg" => "add14_001",
+                "nameOrg" => "МКУ ДО Кантемировский дом детского творчества",
+                "passOrg" => "fbdd5ev9",
+                "district" => 14,
+            ),
+            array(
+                "loginOrg" => "add15_001",
+                "nameOrg" => "МКУ ДО \"Центр детского творчества\"",
+                "passOrg" => "hn42r9ve",
+                "district" => 15,
+            ),
+            array(
+                "loginOrg" => "add16_001",
+                "nameOrg" => "МКУ ДО \"Лискинский Центр развития творчества\"",
+                "passOrg" => "5eahbzn6",
+                "district" => 16,
+            ),
+            array(
+                "loginOrg" => "add16_002",
+                "nameOrg" => "МКУ ДО \"Давыдовский Центр развития творчества\"",
+                "passOrg" => "x9yrdjd4",
+                "district" => 16,
+            ),
+            array(
+                "loginOrg" => "add16_003",
+                "nameOrg" => "МКУ ДО \"Центр технического творчества\"",
+                "passOrg" => "mt7fxhma",
+                "district" => 16,
+            ),
+            array(
+                "loginOrg" => "add17_001",
+                "nameOrg" => "МКУ ДО \"Нижнедевицкий Дом пионеров и школьников\"",
+                "passOrg" => "8a8pdd78",
+                "district" => 17,
+            ),
+            array(
+                "loginOrg" => "add10_001",
+                "nameOrg" => "МКУ ДО \"Дом детского творчества городского округа город Нововоронеж\"",
+                "passOrg" => "8a8pdd78",
+                "district" => 10,
+            ),
+            array(
+                "loginOrg" => "add18_001",
+                "nameOrg" => "МБУДО \"Детско-юношеский центр\"",
+                "passOrg" => "8z3kfdrr",
+                "district" => 18,
+            ),
+            array(
+                "loginOrg" => "add19_001",
+                "nameOrg" => "МКУ ДО \"Станция юных натуралистов\"",
+                "passOrg" => "3ydj2yya",
+                "district" => 19,
+            ),
+            array(
+                "loginOrg" => "add19_002",
+                "nameOrg" => "МКУ ДО \"Центр дополнительного образования детей\"",
+                "passOrg" => "fbfdj84z",
+                "district" => 19,
+            ),
+            array(
+                "loginOrg" => "add20_001",
+                "nameOrg" => "МКУ ДО Ольховатский Дом пионеров и школьников",
+                "passOrg" => "j8r5hssf",
+                "district" => 20,
+            ),
+            array(
+                "loginOrg" => "add21_001",
+                "nameOrg" => "Муниципальное казенное образовательное учреждение дополнительного образования детей станция юных техников",
+                "passOrg" => "e3tdpzz7",
+                "district" => 21,
+            ),
+            array(
+                "loginOrg" => "add21_002",
+                "nameOrg" => "МКУ ДО \"Острогожский центр детского творчества\"",
+                "passOrg" => "93899ssd",
+                "district" => 21,
+            ),
+            array(
+                "loginOrg" => "add21_003",
+                "nameOrg" => "Муниципальное казённое  образовательное учреждение  дополнительного образования детей Коротоякский центр  развития творчества детей и юношества",
+                "passOrg" => "r67cd236",
+                "district" => 21,
+            ),
+            array(
+                "loginOrg" => "add22_001",
+                "nameOrg" => "МКУ ДО \"Павловский Дом детского творчества\"",
+                "passOrg" => "96gyc7ee",
+                "district" => 22,
+            ),
+            array(
+                "loginOrg" => "add22_002",
+                "nameOrg" => "МКУ ДО \"Павловская станция юных техников\"",
+                "passOrg" => "dhcz3y37",
+                "district" => 22,
+            ),
+            array(
+                "loginOrg" => "add22_003",
+                "nameOrg" => "Муниципальное казенное образовательное учреждение дополнительного образования детей Павловская станция юных туристов Павловского муниципального района Воронежской области",
+                "passOrg" => "22274mde",
+                "district" => 22,
+            ),
+            array(
+                "loginOrg" => "add22_004",
+                "nameOrg" => "Муниципальное казённое образовательное учреждение дополнительного образования детей Лосевский Дом детского творчества",
+                "passOrg" => "vzt57u29",
+                "district" => 22,
+            ),
+            array(
+                "loginOrg" => "add22_005",
+                "nameOrg" => "Муниципальное казённое образовательное учреждение дополнительного образования детей Воронцовский Дом детского творчества Павловского муниципального района Воронежской области",
+                "passOrg" => "j5u7jzfv",
+                "district" => 22,
+            ),
+            array(
+                "loginOrg" => "add22_006",
+                "nameOrg" => "МКУ ДО \"Павловская станция юных натуралистов\"",
+                "passOrg" => "63tr5z5v",
+                "district" => 22,
+            ),
+            array(
+                "loginOrg" => "add23_001",
+                "nameOrg" => "МКУ ДО \"Панинский центр детского творчества\"",
+                "passOrg" => "bpu7xjsx",
+                "district" => 23,
+            ),
+            array(
+                "loginOrg" => "add24_001",
+                "nameOrg" => "МКУ ДО Петропавловский Дом детского творчества",
+                "passOrg" => "p5ax94t7",
+                "district" => 24,
+            ),
+            array(
+                "loginOrg" => "add25_001",
+                "nameOrg" => "МКУ ДО \"Центр внешкольной работы\" г. Поворино",
+                "passOrg" => "8ujg9zm6",
+                "district" => 25,
+            ),
+            array(
+                "loginOrg" => "add26_001",
+                "nameOrg" => "МКУ ДО \"Подгоренский Дом детства и юношества\"",
+                "passOrg" => "dgypy8sx",
+                "district" => 26,
+            ),
+            array(
+                "loginOrg" => "add27_001",
+                "nameOrg" => "МКУ ДО \"Дом детского творчества\"",
+                "passOrg" => "mfbjn6gx",
+                "district" => 27,
+            ),
+            array(
+                "loginOrg" => "add27_002",
+                "nameOrg" => "МКУ ДО \"Станция юных натуралистов\"",
+                "passOrg" => "kb5eesuc",
+                "district" => 27,
+            ),
+            array(
+                "loginOrg" => "add28_001",
+                "nameOrg" => "МКУ ДО Репьевского муниципального района Воронежской области \"Центр дополнительного образования\"",
+                "passOrg" => "fd3y6e72",
+                "district" => 28,
+            ),
+            array(
+                "loginOrg" => "add29_001",
+                "nameOrg" => "МКОО ДО Дом детства и юношества г. Россоши",
+                "passOrg" => "7fxb3e7f",
+                "district" => 29,
+            ),
+            array(
+                "loginOrg" => "add29_002",
+                "nameOrg" => "МКОО ДО станция юных натуралистов г. Россоши",
+                "passOrg" => "z8g9sfjy",
+                "district" => 29,
+            ),
+            array(
+                "loginOrg" => "add29_003",
+                "nameOrg" => "МКОО ДО станция юных техников г.Россоши",
+                "passOrg" => "ft7xza3p",
+                "district" => 29,
+            ),
+            array(
+                "loginOrg" => "add30_001",
+                "nameOrg" => "МКУ ДО Семилукский районный Дворец творчества",
+                "passOrg" => "2e46f8vr",
+                "district" => 30,
+            ),
+            array(
+                "loginOrg" => "add30_002",
+                "nameOrg" => "МКУ ДО станция юных техников г. Семилуки Воронежской области",
+                "passOrg" => "6xfkfrz7",
+                "district" => 30,
+            ),
+            array(
+                "loginOrg" => "add31_001",
+                "nameOrg" => "МКОУ ДО Таловский детско-юношеский центр имени Титова Ю.Т.",
+                "passOrg" => "tbyh63g3",
+                "district" => 31,
+            ),
+            array(
+                "loginOrg" => "add32_001",
+                "nameOrg" => "МКУ ДО \"Терновский Дом детского творчества\"",
+                "passOrg" => "kb23pksk",
+                "district" => 32,
+            ),
+            array(
+                "loginOrg" => "add33_001",
+                "nameOrg" => "МБУ ДО \"Дом детского творчества\"",
+                "passOrg" => "53z8tm25",
+                "district" => 33,
+            ),
+            array(
+                "loginOrg" => "add34_001",
+                "nameOrg" => "МКУ ДО \"Эртильский Дом детского творчества\"",
+                "passOrg" => "ectztvb8",
+                "district" => 34,
+            ),
+
+
+        );
+
+        foreach ($user_info as $user) {
+            DB::table('users')->insert([
+                'name' => $user['loginOrg'],
+                'email' => $user['loginOrg'] . '@mail.ru',
+                'fullname' => $user['nameOrg'],
+                'district' => $user['district'],
+                'password' => bcrypt($user['passOrg']),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]);
+        }
+    }
+}
