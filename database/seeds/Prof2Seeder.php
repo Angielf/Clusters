28dcb36c70babfdc029e945704f69ac2597fc766
Angielf <4748510+Angielf@users.noreply.github.com>
2020-11-30 10:40:16 +0300

    Prof2Seeder

---
+++ database/seeds/Prof2Seeder.php
@@ -0,0 +1,257 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Prof2Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user_info = array(
+            array(
+                "loginOrg" => "col_001",
+                "nameOrg" => "ГБПОУ ВО «Воронежский техникум моды и дизайна»",
+                "passOrg" => "c52kpt6t6",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "col_002",
+                "nameOrg" => "ГБПОУ ВО «Воронежский колледж сварки и промышленных технологий»",
+                "passOrg" => "vxfztx2ug",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "col_003",
+                "nameOrg" => "ГБПОУ ВО «Воронежский индустриальный колледж»",
+                "passOrg" => "d9t8t4xrz",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "col_004",
+                "nameOrg" => "ГБПОУ ВО «Воронежский техникум промышленно-строительных технологий»",
+                "passOrg" => "xrdhpad5d",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "col_005",
+                "nameOrg" => "ГБПОУ ВО «Воронежский авиационный техникум имени В.П. Чкалова»",
+                "passOrg" => "z3rh49s83",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "col_006",
+                "nameOrg" => "ГБПОУ ВО «Воронежский государственный промышленно-гуманитарный колледж»",
+                "passOrg" => "6drgdhys6",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "col_007",
+                "nameOrg" => "ГБПОУ ВО «Воронежский государственный промышленно-технологический колледж»",
+                "passOrg" => "4mhvyrv28",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "col_008",
+                "nameOrg" => "ГБПОУ ВО «Воронежский техникум строительных технологий»",
+                "passOrg" => "9z6y4z5fs",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "col_010",
+                "nameOrg" => "ГБПОУ ВО «Воронежский юридический техникум»",
+                "passOrg" => "hncj3y6p9",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "col_011",
+                "nameOrg" => "ГБПОУ ВО «Богучарский аграрно-технологический лицей»",
+                "passOrg" => "hapzfrjbx",
+                "district" => 3,
+            ),
+            array(
+                "loginOrg" => "col_012",
+                "nameOrg" => "ГБПОУ ВО «Верхнемамонский сельскохозяйственный лицей»",
+                "passOrg" => "prm6uubyj",
+                "district" => 6,
+            ),
+            array(
+                "loginOrg" => "col_013",
+                "nameOrg" => "ГБПОУ ВО «Новоусманский многопрофильный техникум»",
+                "passOrg" => "k2gbahycx",
+                "district" => 18,
+            ),
+            array(
+                "loginOrg" => "col_014",
+                "nameOrg" => "ГБПОУ ВО «Поворинский транспортный техникум»",
+                "passOrg" => "63n5xrnu2",
+                "district" => 25,
+            ),
+            array(
+                "loginOrg" => "col_016",
+                "nameOrg" => "ГАПОУ ВО «Новохопёрский аграрно-экономический техникум»",
+                "passOrg" => "e4xcjcv5x",
+                "district" => 19,
+            ),
+            array(
+                "loginOrg" => "col_018",
+                "nameOrg" => "ГБПОУ ВО «Россошанский химико-механический техникум»",
+                "passOrg" => "96ke84k7m",
+                "district" => 29,
+            ),
+            array(
+                "loginOrg" => "col_019",
+                "nameOrg" => "ГБПОУ ВО «Россошанский строительный техникум»",
+                "passOrg" => "gk5eetp2h",
+                "district" => 29,
+            ),
+            array(
+                "loginOrg" => "col_020",
+                "nameOrg" => "ГБПОУ ВО «Россошанский колледж мясной и молочной промышленности»",
+                "passOrg" => "gg4hrv2ap",
+                "district" => 29,
+            ),
+            array(
+                "loginOrg" => "col_021",
+                "nameOrg" => "ГБПОУ ВО «Россошанский техникум сельскохозяйственного и строительного транспорта»",
+                "passOrg" => "zpx5zbta6",
+                "district" => 29,
+            ),
+            array(
+                "loginOrg" => "col_022",
+                "nameOrg" => "ГБПОУ ВО «Аннинский аграрно-промышленный техникум»",
+                "passOrg" => "9x9x2ggab",
+                "district" => 1,
+            ),
+            array(
+                "loginOrg" => "col_023",
+                "nameOrg" => "ГБПОУ ВО «Борисоглебский дорожный техникум»",
+                "passOrg" => "9p6dacc33",
+                "district" => 4,
+            ),
+            array(
+                "loginOrg" => "col_024",
+                "nameOrg" => "ГБПОУ ВО «Борисоглебский технолого-экономический техникум»",
+                "passOrg" => "xb669r7cj",
+                "district" => 4,
+            ),
+            array(
+                "loginOrg" => "col_025",
+                "nameOrg" => "ГБПОУ ВО «Бутурлиновский педагогический техникум»",
+                "passOrg" => "bh9e6bacu",
+                "district" => 5,
+            ),
+            array(
+                "loginOrg" => "col_026",
+                "nameOrg" => "ГБПОУ ВО «Верхнеозерский сельскохозяйственный техникум»",
+                "passOrg" => "5bkasgec7",
+                "district" => 31,
+            ),
+            array(
+                "loginOrg" => "col_027",
+                "nameOrg" => "ГБПОУ ВО «Калачеевский аграрный техникум»",
+                "passOrg" => "yhumd3vkx",
+                "district" => 12,
+            ),
+            array(
+                "loginOrg" => "col_028",
+                "nameOrg" => "ГБПОУ ВО «Острогожский многопрофильный техникум»",
+                "passOrg" => "j63g2svj6",
+                "district" => 21,
+            ),
+            array(
+                "loginOrg" => "col_029",
+                "nameOrg" => "ГБПОУ ВО «Павловский педагогический колледж»",
+                "passOrg" => "24najcygn",
+                "district" => 22,
+            ),
+            array(
+                "loginOrg" => "col_030",
+                "nameOrg" => "ГБПОУ ВО «Павловский техникум» (ГБПОУ ВО «Верхнемамонский сельскохозяйственный лицей»)",
+                "passOrg" => "sjpt792db",
+                "district" => 6,
+            ),
+            array(
+                "loginOrg" => "col_031",
+                "nameOrg" => "ГБПОУ ВО «Россошанский педагогический колледж»",
+                "passOrg" => "tatc93f4e",
+                "district" => 29,
+            ),
+            array(
+                "loginOrg" => "col_033",
+                "nameOrg" => "ГБПОУ ВО «Лискинский промышленно-транспортный техникум имени А.К. Лысенко» (ГБПОУ ВО «Поворинский транспортный техникум»)",
+                "passOrg" => "6jr8dv9te",
+                "district" => 16,
+            ),
+            array(
+                "loginOrg" => "col_034",
+                "nameOrg" => "ГБПОУ ВО «Лискинский аграрно-технологический техникум»",
+                "passOrg" => "jj8uy29y3",
+                "district" => 16,
+            ),
+            array(
+                "loginOrg" => "col_035",
+                "nameOrg" => "ГБПОУ ВО «Воронежский политехнический техникум»",
+                "passOrg" => "TheWr4bar",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "col_036",
+                "nameOrg" => "ГБПОУ ВО «Борисоглебский техникум промышленных и информационных технологий»",
+                "passOrg" => "x4dUDradr",
+                "district" => 4,
+            ),
+            array(
+                "loginOrg" => "col_037",
+                "nameOrg" => "ГБПОУ ВО «Воронежский техникум пищевой и перерабатывающей промышленности» с филиалом в с.Березово Рамонского района",
+                "passOrg" => "b2uPuKuth",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "col_038",
+                "nameOrg" => "ГБПОУ ВО «Борисоглебский сельскохозяйственный техникум» с филиалом  в г. Эртиль",
+                "passOrg" => "ra6rEcraT",
+                "district" => 4,
+            ),
+            array(
+                "loginOrg" => "col_039",
+                "nameOrg" => "ГБПОУ ВО «Семилукский политехнический колледж»",
+                "passOrg" => "ch5SwuxaZ",
+                "district" => 30,
+            ),
+            array(
+                "loginOrg" => "col_040",
+                "nameOrg" => "ГБПОУ ВО «Воронежский государственный промышленно-экономический колледж»",
+                "passOrg" => "masEched5",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "col_041",
+                "nameOrg" => "ГБПОУ ВО «Воронежский государственный профессионально-педагогический колледж»",
+                "passOrg" => "buThec6ew",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "col_042",
+                "nameOrg" => "ГБПОУ ВО  «Бутурлиновский механико-технологический колледж»",
+                "passOrg" => "paF6swUFe",
+                "district" => 5,
+            ),
+        );
+
+        foreach ($user_info as $user) {
+            DB::table('users')->insert([
+                'name' => $user['loginOrg'],
+                'email' => $user['loginOrg'] . '@mail.ru',
+                'fullname' => $user['nameOrg'],
+                'district' => $user['district'],
+                'password' => bcrypt($user['passOrg']),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]);
+        }
+    }
+}
