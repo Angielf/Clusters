3b45880096c6abeba208c4ce0548ab60bd8f7416
Angielf <4748510+Angielf@users.noreply.github.com>
2020-12-17 14:57:54 +0300

    спортивные и художественные организации

---
+++ database/seeds/SportArtSeeder.php
@@ -0,0 +1,276 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SportArtSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user_info = array(
+            array(
+                "loginOrg" => "do6",
+                "nameOrg" => "МКУ ДО Бобровская детско-юношеская спортивная школа",
+                "passOrg" => "caeLu4",
+                "district" => 2,
+            ),
+            array(
+                "loginOrg" => "do700",
+                "nameOrg" => "МКУ «Бобровская спортивная школа «Ледовый дворец имени Вячеслава Фетисова»",
+                "passOrg" => "Ue5oos",
+                "district" => 2,
+            ),
+            array(
+                "loginOrg" => "do16",
+                "nameOrg" => "МКУ ДО \"Богучарская детская юношеская спортивная школа\"",
+                "passOrg" => "vaeK8u",
+                "district" => 3,
+            ),
+            array(
+                "loginOrg" => "do23",
+                "nameOrg" => "МБОУ ДО \"Борисоглебская детско-юношеская спортивная школа\"",
+                "passOrg" => "Cheet2",
+                "district" => 4,
+            ),
+            array(
+                "loginOrg" => "do29",
+                "nameOrg" => "МБОУ ДО Бутурлиновская детско-юношеская спортивная школа",
+                "passOrg" => "En5aif",
+                "district" => 5,
+            ),
+            array(
+                "loginOrg" => "do33",
+                "nameOrg" => "МКУ ДО \"Верхнемамонская детско-юношеская спортивная школа\"",
+                "passOrg" => "Dache5",
+                "district" => 6,
+            ),
+            array(
+                "loginOrg" => "do41",
+                "nameOrg" => "МКУ ДО Верхнехавская детско-юношеская спортивная школа",
+                "passOrg" => "Ahto1e",
+                "district" => 7,
+            ),
+            array(
+                "loginOrg" => "do43",
+                "nameOrg" => "МКУ ДО \"Воробьевская детско-юношеская спортивная школа\"",
+                "passOrg" => "Pigh5u",
+                "district" => 8,
+            ),
+            array(
+                "loginOrg" => "do142",
+                "nameOrg" => "МКУ ДО \"Грибановская детско-юношеская спортивная школа\"",
+                "passOrg" => "Oocho8",
+                "district" => 11,
+            ),
+            array(
+                "loginOrg" => "do148",
+                "nameOrg" => "МКОУ ДО Калачеесвкая детско-юношеская спортивная школа",
+                "passOrg" => "Cei9ah",
+                "district" => 12,
+            ),
+            array(
+                "loginOrg" => "do10",
+                "nameOrg" => "МКУ ДО \"Каменская детско-юношеская спортивная школа им. Г.В. Сушкова\"",
+                "passOrg" => "oof5Se",
+                "district" => 13,
+            ),
+            array(
+                "loginOrg" => "do24",
+                "nameOrg" => "МКУ ДО «Детско-юношеская спортивная школа»",
+                "passOrg" => "Ainga1",
+                "district" => 14,
+            ),
+            array(
+                "loginOrg" => "do28",
+                "nameOrg" => "МКУ ДО \"Каширская детско-юношеская спортивная школа\"",
+                "passOrg" => "Aeve0j",
+                "district" => 15,
+            ),
+            array(
+                "loginOrg" => "do68",
+                "nameOrg" => "МКУ ДО «Детско-юношеская спортивная школа «Восточная»",
+                "passOrg" => "ahMah4",
+                "district" => 16,
+            ),
+            array(
+                "loginOrg" => "do69",
+                "nameOrg" => "МКОУ ДО «Давыдовская детско-юношеская спортивная школа»",
+                "passOrg" => "Ea3mie",
+                "district" => 16,
+            ),
+            array(
+                "loginOrg" => "do70",
+                "nameOrg" => "МКУ ДО «Специализированная ДЮСШ по спортивной гимнастике имени В.С. Растороцкого»",
+                "passOrg" => "Ioch7f",
+                "district" => 16,
+            ),
+            array(
+                "loginOrg" => "do85",
+                "nameOrg" => "МБУ ДО \"Нижнедевицкая детско-юношеская спортивная школа\"",
+                "passOrg" => "uoGhe8",
+                "district" => 17,
+            ),
+            array(
+                "loginOrg" => "do92",
+                "nameOrg" => "МКУ ДО «Детско-юношеская спортивная школа»",
+                "passOrg" => "Eithi1",
+                "district" => 10,
+            ),
+            array(
+                "loginOrg" => "do99",
+                "nameOrg" => "МКУ ДО «Новоусманская детско-юношеская спортивная школа»",
+                "passOrg" => "Shu4oh",
+                "district" => 18,
+            ),
+            array(
+                "loginOrg" => "do104",
+                "nameOrg" => "МКУ ДО «Шуберская детско-юношеская спортивная школа»",
+                "passOrg" => "ohpe4E",
+                "district" => 18,
+            ),
+            array(
+                "loginOrg" => "do113",
+                "nameOrg" => "МКУ ДО «Детско-юношеская спортивная школа»",
+                "passOrg" => "Coh6ra",
+                "district" => 19,
+            ),
+            array(
+                "loginOrg" => "do122",
+                "nameOrg" => "МБУ ДО Ольховатская детско-юношеская спортивная школа",
+                "passOrg" => "beeBu9",
+                "district" => 20,
+            ),
+            array(
+                "loginOrg" => "do131",
+                "nameOrg" => "МКУ ДО Детско-юношеская спортивная школа",
+                "passOrg" => "uRai2e",
+                "district" => 21,
+            ),
+            array(
+                "loginOrg" => "do151",
+                "nameOrg" => "МКОУ ДО Павловская детско-юношеская спортивная школа",
+                "passOrg" => "yoh8La",
+                "district" => 22,
+            ),
+            array(
+                "loginOrg" => "do160",
+                "nameOrg" => "МБОУ ДО «Детско-юношеская спортивная школа «Каисса»",
+                "passOrg" => "Ci9yoh",
+                "district" => 23,
+            ),
+            array(
+                "loginOrg" => "do163",
+                "nameOrg" => "МКУ ДО «Петропавловская детско-юношеская спортивная школа",
+                "passOrg" => "Iph7ti",
+                "district" => 24,
+            ),
+            array(
+                "loginOrg" => "do167",
+                "nameOrg" => "МКУ ДО «Поворинская детско-юношеская спортивная школа»",
+                "passOrg" => "cu5iTh",
+                "district" => 25,
+            ),
+            array(
+                "loginOrg" => "do171",
+                "nameOrg" => "МКУ ДО «Подгоренская ДЮСШ им. заслуженного мастера спорта СССР Думчева Ю.Э.»",
+                "passOrg" => "Adeih2",
+                "district" => 26,
+            ),
+            array(
+                "loginOrg" => "do174",
+                "nameOrg" => "МКУ ДО «Детско-юношеская спортивная школа»",
+                "passOrg" => "iCh0oo",
+                "district" => 27,
+            ),
+            array(
+                "loginOrg" => "do183",
+                "nameOrg" => "МКОО ДО детско-юношеская спортивная школа г. Россошь",
+                "passOrg" => "oB8iep",
+                "district" => 29,
+            ),
+            array(
+                "loginOrg" => "do191",
+                "nameOrg" => "МКУ ДО Семилукская детско-юношеская спортивная школа",
+                "passOrg" => "bH3bPY",
+                "district" => 30,
+            ),
+            array(
+                "loginOrg" => "do207",
+                "nameOrg" => "МКУ ДО Семилукская детско-юношеская спортивная школа единоборств",
+                "passOrg" => "dNzedV",
+                "district" => 30,
+            ),
+            array(
+                "loginOrg" => "do208",
+                "nameOrg" => "МКУ ДО Землянская детско-юношеская спортивная школа",
+                "passOrg" => "qVPW8q",
+                "district" => 30,
+            ),
+            array(
+                "loginOrg" => "do194",
+                "nameOrg" => "МКОУ ДО Таловская детско-юношеская спортивная школа",
+                "passOrg" => "ohLi7e",
+                "district" => 31,
+            ),
+            array(
+                "loginOrg" => "do196",
+                "nameOrg" => "МКУ ДО \"Терновская детско-юношеская спортивная школа\"",
+                "passOrg" => "Yooth3",
+                "district" => 32,
+            ),
+            array(
+                "loginOrg" => "do199",
+                "nameOrg" => "МКОУ ДОД «Детско-юношеская спортивная школа Хохольского района»",
+                "passOrg" => "Eeno7v",
+                "district" => 33,
+            ),
+            array(
+                "loginOrg" => "do205",
+                "nameOrg" => "МКУ ДО \"Эртильская детско-юношеская спортивная школа\"",
+                "passOrg" => "Kat5ye",
+                "district" => 34,
+            ),
+            array(
+                "loginOrg" => "do119",
+                "nameOrg" => "МКУ ДО «Новохоперская детская школа искусств»",
+                "passOrg" => "Bek6jc",
+                "district" => 19,
+            ),
+            array(
+                "loginOrg" => "do179",
+                "nameOrg" => "МКУ ДО \"Репьевская школа искусств\"",
+                "passOrg" => "Pae0fo",
+                "district" => 28,
+            ),
+            array(
+                "loginOrg" => "do201",
+                "nameOrg" => "МБУ ДО \"Хохольская детская школа искусств\"",
+                "passOrg" => "Nae9mo",
+                "district" => 33,
+            ),
+            array(
+                "loginOrg" => "do202",
+                "nameOrg" => "МКУ ДО «Гремяченская детская школа искусств»",
+                "passOrg" => "Phaq0h",
+                "district" => 33,
+            ),
+
+        );
+
+        foreach ($user_info as $user) {
+            DB::table('users')->insert([
+                'name' => $user['loginOrg'],
+                'email' => $user['loginOrg'] . '@mail.ru',
+                'fullname' => $user['nameOrg'],
+                'district' => $user['district'],
+                'password' => bcrypt($user['passOrg']),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]);
+        }
+    }
+}
