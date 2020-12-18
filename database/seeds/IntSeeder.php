c086c4b9619fac00c080c2290da2b555da1fd972
Angielf <4748510+Angielf@users.noreply.github.com>
2020-12-18 10:33:44 +0300

    интернаты

---
+++ database/seeds/IntSeeder.php
@@ -0,0 +1,186 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class IntSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user_info = array(
+            array(
+                "loginOrg" => "brd_001",
+                "nameOrg" => "КОУ ВО для обучающихся с девиантным поведением «Аннинская специальная общеобразовательная школа»",
+                "passOrg" => "vs73pd352",
+                "district" => 1,
+            ),
+            array(
+                "loginOrg" => "brd_002",
+                "nameOrg" => "КОУ ВО «Бобровская школа-интернат для обучающихся с ограниченными возможностями здоровья»",
+                "passOrg" => "fr5n4mz94",
+                "district" => 2,
+            ),
+            array(
+                "loginOrg" => "brd_003",
+                "nameOrg" => "КОУ ВО «Бобровская школа-интернат для детей-сирот и детей, оставшихся без попечения родителей, с ограниченными возможностями здоровья»",
+                "passOrg" => "mtr5g8ks2",
+                "district" => 2,
+            ),
+            array(
+                "loginOrg" => "brd_005",
+                "nameOrg" => "КОУ ВО «Борисоглебская школа-интернат для детей-сирот и детей, оставшихся без попечения родителей, с ограниченными возможностями здоровья»",
+                "passOrg" => "73k3mxkzu",
+                "district" => 4,
+            ),
+            array(
+                "loginOrg" => "brd_006",
+                "nameOrg" => "КОУ ВО «Борисоглебский кадетский корпус»",
+                "passOrg" => "4nb9er8sv",
+                "district" => 4,
+            ),
+            array(
+                "loginOrg" => "brd_007",
+                "nameOrg" => "КОУ ВО «Бутурлиновская школа-интернат для обучающихся с ограниченными возможностями здоровья»",
+                "passOrg" => "9ek8shnry",
+                "district" => 5,
+            ),
+            array(
+                "loginOrg" => "brd_008",
+                "nameOrg" => "КОУ ВО «Верхнемамонская школа-интернат для обучающихся с ограниченными возможностями здоровья»",
+                "passOrg" => "ffpfv7rkp",
+                "district" => 6,
+            ),
+            array(
+                "loginOrg" => "brd_009",
+                "nameOrg" => "КОУ ВО «Верхнемамонский Матвея Платова казачий кадетский корпус»",
+                "passOrg" => "gyjyx5aca",
+                "district" => 6,
+            ),
+            array(
+                "loginOrg" => "brd_010",
+                "nameOrg" => "КОУ ВО «Руднянская школа-интернат для обучающихся с ограниченными возможностями здоровья»",
+                "passOrg" => "cjb322xt7",
+                "district" => 8,
+            ),
+            array(
+                "loginOrg" => "brd_011",
+                "nameOrg" => "КОУ ВО «Центр лечебной педагогики и дифференцированного обучения»",
+                "passOrg" => "nmf2gzn4c",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "brd_012",
+                "nameOrg" => "КОУ ВО «Воронежская школа № 31 для обучающихся с ограниченными возможностями здоровья»",
+                "passOrg" => "c3h3kfth7",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "brd_013",
+                "nameOrg" => "КОУ ВО «Воронежская  школа-интернат № 3 для обучающихся с ограниченными возможностями здоровья»",
+                "passOrg" => "7me23hyts",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "brd_014",
+                "nameOrg" => "КОУ ВО «Воронежская школа-интернат № 6 для обучающихся с ограниченными возможностями здоровья»",
+                "passOrg" => "dhyxmb7vh",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "brd_015",
+                "nameOrg" => "КОУ ВО «Воронежская школа-интернат № 7 для обучающихся с ограниченными возможностями здоровья»",
+                "passOrg" => "amb53js3g",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "brd_017",
+                "nameOrg" => "КОУ ВО «Воронежский центр психолого-педагогической реабилитации и коррекции»",
+                "passOrg" => "u48pka5dy",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "brd_018",
+                "nameOrg" => "КОУ ВО «Михайловский кадетский корпус»",
+                "passOrg" => "52pg3hjdm",
+                "district" => 45,
+            ),
+            array(
+                "loginOrg" => "brd_020",
+                "nameOrg" => "КОУ ВО «Новоусманская школа-интернат для обучающихся с ограниченными возможностями здоровья»",
+                "passOrg" => "yyebabn7m",
+                "district" => 18,
+            ),
+            array(
+                "loginOrg" => "brd_021",
+                "nameOrg" => "КОУ ВО «Хреновская  школа-интернат для детей-сирот и детей, оставшихся без попечения родителей, с ограниченными возможностями здоровья»",
+                "passOrg" => "rghfkke6d",
+                "district" => 18,
+            ),
+            array(
+                "loginOrg" => "brd_022",
+                "nameOrg" => "КОУ ВО «Краснянская школа-интернат для обучающихся с ограниченными возможностями здоровья»",
+                "passOrg" => "pufc2kyyu",
+                "district" => 19,
+            ),
+            array(
+                "loginOrg" => "brd_023",
+                "nameOrg" => "КОУ ВО «Елань-Коленовский центр психолого-педагогической, медицинской и социальной помощи»",
+                "passOrg" => "uk9ckej4c",
+                "district" => 19,
+            ),
+            array(
+                "loginOrg" => "brd_024",
+                "nameOrg" => "КОУ ВО «Новоосиновская школа-интернат для обучающихся с ограниченными возможностями здоровья»",
+                "passOrg" => "9sc8ptd2z",
+                "district" => 21,
+            ),
+            array(
+                "loginOrg" => "brd_025",
+                "nameOrg" => "КОУ ВО «Павловская школа-интернат №2 для обучающихся с ограниченными возможностями здоровья»",
+                "passOrg" => "xmty47bf9",
+                "district" => 22,
+            ),
+            array(
+                "loginOrg" => "brd_026",
+                "nameOrg" => "КОУ ВО «Павловская школа-интернат №1 для обучающихся с ограниченными возможностями здоровья»",
+                "passOrg" => "75brsbub3",
+                "district" => 22,
+            ),
+            array(
+                "loginOrg" => "brd_027",
+                "nameOrg" => "КОУ ВО «Горожанский казачий кадетский корпус»",
+                "passOrg" => "84jzfgyu5",
+                "district" => 27,
+            ),
+            array(
+                "loginOrg" => "brd_029",
+                "nameOrg" => "КОУ ВО «Землянская школа-интернат для обучающихся с ограниченными возможностями здоровья»",
+                "passOrg" => "krn9ggrt3",
+                "district" => 30,
+            ),
+            array(
+                "loginOrg" => "brd_029",
+                "nameOrg" => "КОУ ВО «Семилукский центр психолого-педагогической, медицинской и социальной помощи»",
+                "passOrg" => "krn9ggrt3",
+                "district" => 30,
+            ),
+
+        );
+
+        foreach ($user_info as $user) {
+            DB::table('users')->insert([
+                'name' => $user['loginOrg'],
+                'email' => $user['loginOrg'] . '@mail.ru',
+                'fullname' => $user['nameOrg'],
+                'district' => $user['district'],
+                'password' => bcrypt($user['passOrg']),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]);
+        }
+    }
+}
